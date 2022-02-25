4c913b7435af4ee7c8be593dab26412b55b96a5b
matrixsave <kevin.willem@epitech.eu>
2022-02-25 11:34:43 +0100

    [ADD] added notebooks code in train model

--- DataScienceTek5/src/data/make_dataset.py
+++ DataScienceTek5/src/data/make_dataset.py
@@ -3,16 +3,56 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.model_selection import StratifiedShuffleSplit
+import matplotlib.pyplot as plt
+import sys
+import os
 
+path = os.getcwd() + "/src"
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+sys.path.append(path)
+from models.train_model import train
+
+#@click.command()
+#@click.argument('input_filepath', type=click.Path(exists=True))
+#@click.argument('output_filepath', type=click.Path())
+
+def load_data():
+    olivetti = fetch_olivetti_faces()
+    return olivetti
+
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
+
+
+    dataset = load_data()
+    sss = StratifiedShuffleSplit()
+    sss.get_n_splits(dataset.data, dataset.target)
+
+    strat_split = StratifiedShuffleSplit(n_splits=1, test_size=40, random_state=42)
+    train_valid_idx, test_idx = next(strat_split.split(dataset.data, dataset.target))
+    X_train_valid = dataset.data[train_valid_idx]
+    y_train_valid = dataset.target[train_valid_idx]
+    X_test = dataset.data[test_idx]
+    y_test = dataset.target[test_idx]
+
+    strat_split = StratifiedShuffleSplit(n_splits=1, test_size=80, random_state=43)
+    train_idx, valid_idx = next(strat_split.split(X_train_valid, y_train_valid))
+    X_train = X_train_valid[train_idx]
+    y_train = y_train_valid[train_idx]
+    X_valid = X_train_valid[valid_idx]
+    y_valid = y_train_valid[valid_idx]
+
+    train(X_train_valid, X_train, X_valid, X_test, y_train, y_valid)
+    #print(X_train.shape, y_train.shape)
+    #print(X_valid.shape, y_valid.shape)
+    #print(X_test.shape, y_test.shape)
+
+    #print(dataset)
     logger.info('making final data set from raw data')
 
 
@@ -27,4 +67,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    main("../models/train_model.py", "../models/test_model.py")
---
+++ DataScienceTek5/src/models/train_model.py
@@ -0,0 +1,132 @@
+import pickle
+
+from skimage import exposure
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+
+
+def train(X_train_valid, X_train, X_valid, X_test, y_train, y_valid):
+    img = exposure.equalize_adapthist(X_train_valid[4], clip_limit=0.01)
+
+    plt.figure(figsize=(7, 4))
+    plt.subplot(121)
+    #plt.imshow(X_train_valid[4].reshape(-1, 64), cmap="gray")
+    plt.title("Original", fontsize=16)
+    plt.subplot(122)
+    #plt.imshow(img.reshape(-1, 64), cmap="gray")
+    plt.title("Color histogramme", fontsize=16)
+    print("Finished")
+
+    pca = PCA()
+    pca.fit(X_train)
+    variance_rate = 0.99
+    cumsum = np.cumsum(pca.explained_variance_ratio_)
+    d = np.argmax(cumsum >= variance_rate) + 1
+
+    plt.figure(figsize=(6,4))
+    plt.plot(cumsum, linewidth=3)
+    plt.axis([0, 400, 0, 1])
+    plt.xlabel("Dimensions")
+    plt.ylabel("Explained Variance")
+    plt.plot([d, d], [0, variance_rate], "k:")
+    plt.plot([0, d], [variance_rate, variance_rate], "k:")
+    plt.plot(d, variance_rate, "ko")
+    plt.annotate("Elbow", xy=(65, 0.85), xytext=(70, 0.7),
+                arrowprops=dict(arrowstyle="->"), fontsize=16)
+    plt.grid(True)
+
+    pca = PCA(variance_rate)
+
+    X_train_pca = pca.fit_transform(X_train)
+
+    X_valid_pca = pca.transform(X_valid)
+
+    X_test_pca = pca.transform(X_test)
+
+    X_recovered = pca.inverse_transform(X_train_pca)
+
+    plt.figure(figsize=(7, 4))
+    plt.subplot(121)
+    #plt.imshow(X_train[1].reshape(-1, 64), cmap="gray")
+    plt.title("Original", fontsize=16)
+    plt.subplot(122)
+    #plt.imshow(X_recovered[1].reshape(-1, 64), cmap="gray")
+    plt.title("Compressed", fontsize=16)
+
+    k_range = range(5, 150, 5)
+    kmeans_per_k = []
+    for k in k_range:
+        print("k={}".format(k))
+        kmeans = KMeans(n_clusters=k, random_state=42).fit(X_train_pca)
+        kmeans_per_k.append(kmeans)
+    
+    
+
+    silhouette_scores = [silhouette_score(X_train_pca, model.labels_)
+                        for model in kmeans_per_k]
+    best_index = np.argmax(silhouette_scores)
+    best_k = k_range[best_index]
+    best_score = silhouette_scores[best_index]
+
+    plt.figure(figsize=(8, 3))
+    plt.plot(k_range, silhouette_scores, "bo-")
+    plt.xlabel("$k$", fontsize=14)
+    plt.ylabel("Silhouette score", fontsize=14)
+    plt.plot(best_k, best_score, "rs")
+    print(best_k)
+    best_model = kmeans_per_k[best_index]
+    #plt.show()
+
+    def plot_faces(faces, labels, n_cols=5):
+        faces = faces.reshape(-1, 64, 64)
+        n_rows = (len(faces) - 1) // n_cols + 1
+        plt.figure(figsize=(n_cols, n_rows * 1.1))
+        for index, (face, label) in enumerate(zip(faces, labels)):
+            plt.subplot(n_rows, n_cols, index + 1)
+            plt.imshow(face, cmap="gray")
+            plt.axis("off")
+            plt.title(label)
+        #plt.show()
+
+    for cluster_id in np.unique(best_model.labels_):
+        print("Cluster", cluster_id)
+        in_cluster = best_model.labels_==cluster_id
+        faces = X_train[in_cluster]
+        labels = y_train[in_cluster]
+        plot_faces(faces, labels)
+    
+    clf = RandomForestClassifier(n_estimators=150, random_state=42)
+    clf.fit(X_train_pca, y_train)
+    clf.score(X_valid_pca, y_valid)
+    X_train_reduced = best_model.transform(X_train_pca)
+    X_valid_reduced = best_model.transform(X_valid_pca)
+    X_test_reduced = best_model.transform(X_test_pca)
+
+    clf = RandomForestClassifier(n_estimators=150, random_state=42)
+    clf.fit(X_train_reduced, y_train)
+        
+    clf.score(X_valid_reduced, y_valid)
+
+    X_train_reduced.shape
+
+    for n_clusters in k_range:
+        pipeline = Pipeline([
+            ("kmeans", KMeans(n_clusters=n_clusters, random_state=42)),
+            ("forest_clf", RandomForestClassifier(n_estimators=150, random_state=42))
+        ])
+        pipeline.fit(X_train_pca, y_train)
+        print(n_clusters, pipeline.score(X_valid_pca, y_valid))
+    X_train_extended = np.c_[X_train_pca, X_train_reduced]
+    X_valid_extended = np.c_[X_valid_pca, X_valid_reduced]
+    X_test_extended = np.c_[X_test_pca, X_test_reduced]
+    clf = RandomForestClassifier(n_estimators=150, random_state=42)
+    clf.fit(X_train_extended, y_train)
+    clf.score(X_valid_extended, y_valid)
+
+def dump_model(model):
+    pickle.dump(model, open('./models/rdf_nestimators_150_state_42.pkl', 'wb'))
